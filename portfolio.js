d45f10e17a8b1adc6893e2d05321532e2d5ac569
Akari Yamabuki <guranytou@gmail.com>
2018-09-21 23:18:16 +0900

    FizzBuzz追加

--- portfolio.js
+++ portfolio.js
@@ -1,5 +1,8 @@
 $(document).ready(function () {
     $('.works1Img').click(function(){
         $('.works1Description').slideToggle();
+    });
+    $('.works2Img').click(function(){
+        $('.works2Description').slideToggle();
     })
 });
